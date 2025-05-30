d95557b1ddccb8534e2548c8eeefba434d5f8cfa
CoderVu <vunguyen.170803@gmail.com>
2025-05-30 07:52:02 +0700

    delete docker file

--- src/main/java/com/example/CapstoneProject/security/AzureBlobStorageConfig.java
+++ src/main/java/com/example/CapstoneProject/security/AzureBlobStorageConfig.java
@@ -10,8 +10,8 @@ public class AzureBlobStorageConfig {
 
     @Bean
     public BlobServiceClient blobServiceClient() {
-        String AccountKey = System.getenv("AZURE_STORAGE_ACCOUNT_KEY").trim();
-//        String AccountKey = "";
+       // String AccountKey = System.getenv("AZURE_STORAGE_ACCOUNT_KEY").trim();
+     String AccountKey = "Mmheb6KKumzJiWclGO937G5pEqGihUBeuMYhwXixVnCLx13dguUeCbsX5J8OhZIHuHrCiT/LD96Y+AStjTASxQ==";
         if (AccountKey.isEmpty()) {
             throw new IllegalArgumentException("Azure Storage Account Key is missing.");
         }
